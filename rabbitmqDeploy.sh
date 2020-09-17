db5a94d485304dcdaf9a98e6e3137b0f8bf2f287
Aditi <aggarwal.aditi97@gmail.com>
2020-09-17 14:21:49 +0530

    [EKA-944] | Gayatri/Aditi | Add dev-service queue

--- rabbitmqDeploy.sh
+++ rabbitmqDeploy.sh
@@ -2,6 +2,9 @@
 # Adding the consent manager queue
 curl https://raw.githubusercontent.com/ProjectEKA/consent-manager/master/rabbitmq/cm_rabbitmq.json -o ./rabbitmq/cm_queues.json
 python ./rabbitmq/rabbitmqsetup.py ./rabbitmq/cm_queues.json ./rabbitmq/definitions.json
+# Adding the dev service queue
+curl https://raw.githubusercontent.com/ProjectEKA/dev-service/master/rabbitmq/devservice_rabbitmq.json -o ./rabbitmq/devservice_queues.json
+python ./rabbitmq/rabbitmqsetup.py ./rabbitmq/devservice_queues.json ./rabbitmq/definitions.json
 #Adding gateway queue
 curl https://raw.githubusercontent.com/ProjectEKA/gateway/master/rabbitmq/gateway_rabbitmq.json -o ./rabbitmq/gateway_queues.json
 python ./rabbitmq/rabbitmqsetup.py ./rabbitmq/gateway_queues.json ./rabbitmq/definitions.json
